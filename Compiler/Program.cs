3d2740ed66efa5dafcb05327f61eb80ad12c1d2d
sh f <1519755150@qq.com>
2018-09-25 15:57:05 +0800

    输出中间代码

--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -13,6 +13,19 @@ namespace Compiler
         public static void Main(string[] arg)
         {
             string code = File.ReadAllText($"../../test.pl0");
+            ILGenerator ilg = new ILGenerator();
+            ilg.GenerateCode(code, 0);
+            ilg.PrintCode();
+            Console.WriteLine("按任意键继续");
+            Console.ReadKey();
+
+            PCodeGeneraotr pcg = new PCodeGeneraotr();
+            pcg.GenerateCode(code, 0);
+            pcg.PrintCode();
+            Console.WriteLine("按任意键开始执行");
+            Console.ReadKey();
+
+
             VirtualMachine vm = new VirtualMachine();
             vm.Run(code, 0);
         }
