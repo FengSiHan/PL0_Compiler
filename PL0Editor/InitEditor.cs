23e5e3bdd83159113a6b14b0c6c4d71a68395f6d
sh f <1519755150@qq.com>
2018-09-27 17:27:52 +0800

    引用AvalonEdit源码

--- PL0Editor/InitEditor.cs
+++ PL0Editor/InitEditor.cs
@@ -11,22 +11,120 @@ using System.IO;
 using ICSharpCode.AvalonEdit.Search;
 using Microsoft.Win32;
 using System.Reflection;
+using System.Xml;
+using System.Windows.Media;
+using System.Windows.Input;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Editing;
+using System.Windows.Threading;
+
 namespace PL0Editor
 {
-    public class InitEditor
-    {
-        public static TextEditor Target { get; set; }
 
-        private static void InitialHighlight()
+    public class MyCompletionData : ICompletionData
+    {
+        public MyCompletionData(string text)
         {
-            using (Stream stream = new FileStream("PL0.xshd",FileMode.Open))
+            this.Text = text;
+        }
+
+        public System.Windows.Media.ImageSource Image
+        {
+            get { return null; }
+        }
+
+        public string Text { get; private set; }
+
+        // Use this property if you want to show a fancy UIElement in the drop down list.
+        public object Content
+        {
+            get { return this.Text; }
+        }
+
+        public object Description
+        {
+            get { return "Description for " + this.Text; }
+        }
+
+        public double Priority { get { return 0; } }
+
+        public void Complete(TextArea textArea, ISegment completionSegment, EventArgs insertionRequestEventArgs)
+        {
+            textArea.Document.Replace(completionSegment, this.Text);
+        }
+        
+    }
+    public class BraceFoldingStrategy
+    {
+        /// <summary>
+        /// Gets/Sets the opening brace. The default value is '{'.
+        /// </summary>
+        public char OpeningBrace { get; set; }
+
+        /// <summary>
+        /// Gets/Sets the closing brace. The default value is '}'.
+        /// </summary>
+        public char ClosingBrace { get; set; }
+
+        /// <summary>
+        /// Creates a new BraceFoldingStrategy.
+        /// </summary>
+        public BraceFoldingStrategy()
+        {
+            this.OpeningBrace = '{';
+            this.ClosingBrace = '}';
+        }
+
+        public void UpdateFoldings(FoldingManager manager, TextDocument document)
+        {
+            int firstErrorOffset;
+            IEnumerable<NewFolding> newFoldings = CreateNewFoldings(document, out firstErrorOffset);
+            manager.UpdateFoldings(newFoldings, firstErrorOffset);
+        }
+
+        /// <summary>
+        /// Create <see cref="NewFolding"/>s for the specified document.
+        /// </summary>
+        public IEnumerable<NewFolding> CreateNewFoldings(TextDocument document, out int firstErrorOffset)
+        {
+            firstErrorOffset = -1;
+            return CreateNewFoldings(document);
+        }
+
+        /// <summary>
+        /// Create <see cref="NewFolding"/>s for the specified document.
+        /// </summary>
+        public IEnumerable<NewFolding> CreateNewFoldings(ITextSource document)
+        {
+            List<NewFolding> newFoldings = new List<NewFolding>();
+
+            Stack<int> startOffsets = new Stack<int>();
+            int lastNewLineOffset = 0;
+            char openingBrace = this.OpeningBrace;
+            char closingBrace = this.ClosingBrace;
+            for (int i = 0; i < document.TextLength; i++)
             {
-                if (stream is null)
+                char c = document.GetCharAt(i);
+                if (c == openingBrace)
                 {
-                     
+                    startOffsets.Push(i);
+                }
+                else if (c == closingBrace && startOffsets.Count > 0)
+                {
+                    int startOffset = startOffsets.Pop();
+                    // don't fold if opening and closing brace are on the same line
+                    if (startOffset < lastNewLineOffset)
+                    {
+                        newFoldings.Add(new NewFolding(startOffset, i + 1));
+                    }
+                }
+                else if (c == '\n' || c == '\r')
+                {
+                    lastNewLineOffset = i + 1;
                 }
             }
+            newFoldings.Sort((a, b) => a.StartOffset.CompareTo(b.StartOffset));
+            return newFoldings;
         }
-        private static 
     }
 }
--- PL0Editor/MainWindow.xaml.cs
+++ PL0Editor/MainWindow.xaml.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Documents;
 using System.Windows.Input;
 using Compiler;
 using Microsoft.Win32;
 using MahApps.Metro.Controls;
-using System.Threading;
 using System.IO;
-using System.Text;
 using System.Windows.Media;
+using ICSharpCode.AvalonEdit.Folding;
+using System;
+using ICSharpCode.AvalonEdit.CodeCompletion;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Search;
+using System.Xml;
+using System.Windows.Threading;
 
 namespace PL0Editor
 {
@@ -21,11 +25,89 @@ namespace PL0Editor
         public MainWindow()
         {
             InitializeComponent();
+            Init();
+            SearchPanel.Install(CodeEditor);
+            foldingManager = FoldingManager.Install(CodeEditor.TextArea);
+            DispatcherTimer foldingUpdateTimer = new DispatcherTimer();
+            foldingUpdateTimer.Interval = TimeSpan.FromSeconds(2);
+            foldingUpdateTimer.Tick += (i, j) =>
+            {
+                foldingStrategy.UpdateFoldings(foldingManager, CodeEditor.Document);
+            };
+            foldingUpdateTimer.Start();
+            CodeEditor.ShowLineNumbers = true;
+            CodeEditor.Options.HighlightCurrentLine = true;
+            CodeEditor.Options.ConvertTabsToSpaces = true;
+            RowText.DataContext = CodeEditor.TextArea.Caret;
+            ColText.DataContext = CodeEditor.TextArea.Caret;
         }
+        BraceFoldingStrategy foldingStrategy = new BraceFoldingStrategy();
+        FoldingManager foldingManager;
+        CompletionWindow completionWindow;
 
+
+        public void Init()
+        {
+            IHighlightingDefinition customHighlighting;
+            using (Stream stream = new FileStream("../../PL0.xshd", FileMode.Open))
+            {
+                if (stream is null)
+                {
+                    throw new Exception("PL0 highlight ruleset is lost");
+                }
+                else
+                {
+                    using (XmlReader reader = new XmlTextReader(stream))
+                    {
+                        customHighlighting = ICSharpCode.AvalonEdit.Highlighting.Xshd.
+                            HighlightingLoader.Load(reader, HighlightingManager.Instance);
+                    }
+                };
+            }
+
+            HighlightingManager.Instance.RegisterHighlighting("PL0 Highlighting", new string[] { ".PL0", ".pl0" }, customHighlighting);
+
+            CodeEditor.SyntaxHighlighting = customHighlighting;
+
+            SetValue(TextOptions.TextFormattingModeProperty, TextFormattingMode.Display);
+
+            CodeEditor.TextArea.TextEntering += CodeEditor_TextArea_TextEntering;
+            CodeEditor.TextArea.TextEntered += CodeEditor_TextArea_TextEntered;
+        }
+        public void CodeEditor_TextArea_TextEntered(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text == ".")
+            {
+                // open code completion after the user has pressed dot:
+                completionWindow = new CompletionWindow(CodeEditor.TextArea);
+                // provide AvalonEdit with the data:
+                IList<ICompletionData> data = completionWindow.CompletionList.CompletionData;
+                data.Add(new MyCompletionData("Item1"));
+                data.Add(new MyCompletionData("Item2"));
+                data.Add(new MyCompletionData("Item3"));
+                data.Add(new MyCompletionData("Another item"));
+                completionWindow.Show();
+                completionWindow.Closed += delegate
+                {
+                    completionWindow = null;
+                };
+            }
+        }
+        public void CodeEditor_TextArea_TextEntering(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text.Length > 0 && completionWindow != null)
+            {
+                if (!char.IsLetterOrDigit(e.Text[0]))
+                {
+                    // Whenever a non-letter is typed while the completion window is open,
+                    // insert the currently selected element.
+                    completionWindow.CompletionList.RequestInsertion(e);
+                }
+            }
+            // do not set e.Handled=true - we still want to insert the character that was typed
+        }
         private void Save_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            CodeEditor
         }
 
         private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
@@ -35,7 +117,7 @@ namespace PL0Editor
             bool? result = dialog.ShowDialog();
             if (result.Value)
             {
-                
+
             }
         }
 
@@ -46,7 +128,11 @@ namespace PL0Editor
             parser.Parse();
             ErrorList.ItemsSource = parser.ErrorMsg.Errors;
         }
-        
-        
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            RowText.Text = CodeEditor.TextArea.Caret.Line.ToString();
+            
+        }
     }
 }
